35e5606ec4c45379c987559817a605a4021bdbc9
soknyvan <soknyvann3@gmail.com>
2020-09-28 15:25:17 +0700

    added vendor edit to myfile

---
+++ myfile/backpack/crud/src/resources/views/base/inc/sidebar_content.blade.php
@@ -0,0 +1,5 @@
+<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+<li class="nav-item nav-dropdown-toggle"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i
+            class="la la-home nav-icon"></i>
+        {{ trans('backpack::base.dashboard') }}</a></li>
+<!-- Users, Roles, Permissions -->
